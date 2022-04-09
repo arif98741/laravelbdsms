67f4170a5f3d5ca0443071d7f6da3b8dc6d11732
Ariful <arif98741@gmail.com>
2022-04-09 06:36:39 +0600

    Add Mobishasra Provider to list for sending message to BD

---
+++ src/Provider/Mobishasra.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ *  Last Modified: 6/29/21, 12:06 AM
+ *  Copyright (c) 2021
+ *  -created by Ariful Islam
+ *  -All Rights Preserved By
+ *  -If you have any query then knock me at
+ *  arif98741@gmail.com
+ *  See my profile @ https://github.com/arif98741
+ */
+
+namespace Xenon\LaravelBDSms\Provider;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Xenon\LaravelBDSms\Handler\ParameterException;
+use Xenon\LaravelBDSms\Sender;
+
+class Mobishasra extends AbstractProvider
+{
+    /**
+     * BulkSmsBD constructor.
+     * @param Sender $sender
+     */
+    public function __construct(Sender $sender)
+    {
+        $this->senderObject = $sender;
+    }
+
+    /**
+     * Send Request To Api and Send Message
+     * @throws GuzzleException
+     */
+    public function sendRequest()
+    {
+        $number = $this->senderObject->getMobile();
+        $text = $this->senderObject->getMessage();
+        $config = $this->senderObject->getConfig();
+
+        $client = new Client([
+            'base_uri' => 'https://mshastra.com/sendurlcomma.aspx',
+            'timeout' => 10.0,
+        ]);
+
+        $response = $client->request('GET', '', [
+            'query' => [
+                'user' => $config['user'],
+                'pwd' => $config['pwd'],
+                'senderid' => $config['senderid'],
+                'mobileno' => '88' . $number,
+                'msgtext' => $text,
+                'priority' => 'High',
+                'CountryCode' => 'ALL',
+            ],
+            'verify' => false
+        ]);
+
+        $body = $response->getBody();
+
+        $smsResult = $body->getContents();
+
+
+        $data['number'] = $number;
+        $data['message'] = $text;
+        $report = $this->generateReport($smsResult, $data);
+        return $report->getContent();
+    }
+
+    /**
+     * @return void
+     * @throws ParameterException
+     */
+    public function errorException()
+    {
+        if (!array_key_exists('user', $this->senderObject->getConfig())) {
+            throw new ParameterException('user key is absent in configuration');
+        }
+        if (!array_key_exists('pwd', $this->senderObject->getConfig())) {
+            throw new ParameterException('pwd key is absent in configuration');
+        }
+        if (!array_key_exists('senderid', $this->senderObject->getConfig())) {
+            throw new ParameterException('senderid key is absent in configuration');
+        }
+    }
+}
