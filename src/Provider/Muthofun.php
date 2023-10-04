ad932f599d3d149e1276afd86d964c1247a2c380
Ariful Islam <arif98741@gmail.com>
2023-10-05 00:40:56 +0600

    :fire: new muthofun sms gateway added

---
+++ src/Provider/Muthofun.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ *  Last Modified: 10/04/23, 11:50 PM
+ *  Copyright (c) 2023
+ *  -created by Ariful Islam
+ *  -All Rights Preserved By
+ *  -If you have any query then knock me at
+ *  arif98741@gmail.com
+ *  See my profile @ https://github.com/arif98741
+ */
+
+namespace Xenon\LaravelBDSms\Provider;
+
+use Xenon\LaravelBDSms\Handler\RenderException;
+use Xenon\LaravelBDSms\Request;
+use Xenon\LaravelBDSms\Sender;
+
+class Muthofun extends AbstractProvider
+{
+    /**
+     * Muthofun constructor.
+     * @param Sender $sender
+     */
+    public function __construct(Sender $sender)
+    {
+        $this->senderObject = $sender;
+    }
+
+    /**
+     * Send Request To Api and Send Message
+     * @throws RenderException
+     */
+    public function sendRequest()
+    {
+        $mobile = $this->senderObject->getMobile();
+        $text = $this->senderObject->getMessage();
+        $config = $this->senderObject->getConfig();
+        $queue = $this->senderObject->getQueue();
+
+        $query = [
+            'sender_id' => $config['sender_id'],
+            'remove_duplicate' => true,
+            'receiver' => $mobile,
+            'message' => $text,
+        ];
+
+        if (is_array($mobile)) {
+            $mobile = implode(',', $mobile);
+        }
+
+        $requestObject = new Request('https://sysadmin.muthobarta.com/api/v1/send-sms', $query, $queue);
+
+        if (!str_starts_with($config['api_key'], "Token ")) {
+            $config['api_key'] = "Token " . $config['api_key'];
+        }
+        $requestObject->setHeaders(['Authorization' => $config['api_key']])->setContentTypeJson(true);
+
+        $response = $requestObject->post();
+        if ($queue) {
+            return true;
+        }
+        $body = $response->getBody();
+        $smsResult = $body->getContents();
+        $data['number'] = $mobile;
+        $data['message'] = $text;
+        return $this->generateReport($smsResult, $data)->getContent();
+    }
+
+    /**
+     * @throws RenderException
+     */
+    public function errorException(): void
+    {
+        if (!array_key_exists('sender_id', $this->senderObject->getConfig())) {
+            throw new RenderException('sender_id key is absent in configuration');
+        }
+        if (!array_key_exists('api_key', $this->senderObject->getConfig())) {
+            throw new RenderException('api_key key is absent in configuration');
+        }
+
+    }
+}
